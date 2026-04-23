27dbfcba308d7f68465fddc7e955bc55df2a9ca3
canbedran356-code <canbedran356@gmail.com>
2026-04-23 03:15:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,22 +2,6 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-from flask import Flask
-from threading import Thread
-
-app_web = Flask('')
-
-@app_web.route('/')
-def home():
-    return "Bot aktif!"
-
-def run():
-    app_web.run(host='0.0.0.0', port=10000)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
 TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -26,5 +10,4 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-keep_alive()
 app.run_polling()
